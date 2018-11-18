182475d61da05e28b8fb5008b60fe5c85a3d6d9f
hilal <hilaldnmez@yahoo.com>
2018-11-19 01:06:40 +0300

    viterbi implementation

--- pos_tagger.py
+++ pos_tagger.py
@@ -3,6 +3,7 @@ import pandas as pd
 import re
 from collections import defaultdict
 import operator
+from sklearn.model_selection import train_test_split
 
 
 input_file = 'Project (Application 1) (MetuSabanci Treebank).conll'
@@ -84,6 +85,26 @@ def create_matrices(pre_sentence,len_tags,len_words):
             current_id = word_tag_pair[1]
             transition_matrix[previous_id][current_id] = transition_matrix[previous_id][current_id] + 1
             previous_id = current_id
+
+
+    
+    #add start and end node in transition matrix
+    temp1 = np.zeros((1,len_tags))
+    temp2 = np.zeros((len_tags+1,1))
+    for sentence in pre_sentence:
+        first_tag_id = sentence[0][1]
+        last_tag_id = sentence[-1][1]
+        temp1[0][first_tag_id] = temp1[0][first_tag_id] + 1 
+        temp2[last_tag_id][0] = temp2[last_tag_id][0] + 1 
+        
+    # start node in row , end node in column    
+    transition_matrix = np.vstack((transition_matrix, temp1))
+    transition_matrix = np.hstack((transition_matrix, temp2))
+    
+    # convert probability matrix
+    observation_matrix = observation_matrix / np.sum(observation_matrix, axis=0)
+    transition_matrix = transition_matrix / np.sum(transition_matrix, axis=1).reshape((len_tags+1,1))
+
             
     return transition_matrix,observation_matrix
 
@@ -106,49 +127,81 @@ def generate_unknown_prob():
 
 
     
-# Not ready now
-# viterbi sadece testte kullanılacak    
-def viterbi_no_smoothing():
-    viterbi_base_matrix = np.zeros((len_tags+2,len_tags))
-    for sentence in pre_sentence:
-        first_tag_id = sentence[0][1]
-        last_tag_id = sentence[-1][1]
-        viterbi_base_matrix[0][first_tag_id] = viterbi_base_matrix[0][first_tag_id] + 1 
-        viterbi_base_matrix[1][last_tag_id] = viterbi_base_matrix[1][last_tag_id] + 1 
+# viterbi is used for only test sequence
+# sequence consists of id of the word in the sentence    
+def viterbi_algorithm(sequence):
+    T  = len(sequence) # number of word in sequence
+    word_id = sequence[0] # word_id  = id of the first word
     
-    viterbi_base_matrix[0] = viterbi_base_matrix[0] / np.sum(viterbi_base_matrix[0])
-    viterbi_base_matrix[-1] = viterbi_base_matrix[-1] / np.sum(viterbi_base_matrix[-1])  
-
-
-
-
-
-
-sentence_list = get_original_sentences(input_file)
-pre_sentence_list,tag_list,word_list = preprocessing(sentence_list)
-tags, words = give_id(tag_list,word_list)
-pre_sentence = convert_id(pre_sentence_list,tags,words)
-len_tags = len(tags)
-len_words = len(words)
-
-transition_matrix,observation_matrix = create_matrices(pre_sentence,len_tags,len_words)
-
-
-
-#%%
-
-
+    viterbi = np.zeros((len_tags+2,T+1))
+    backpointer = np.zeros((len_tags+2,T+1))
     
+    # start state is full
+    # -1 means the start state(node) in backpointer
+    for state in range(len_tags):
+        viterbi[state][0] = transition_matrix[len_tags][state] * observation_matrix[word_id][state]
+        backpointer[state][0] = -1
+
+    for t in range(1,T):
+        word_id = sequence[t]
+        for s in range(len_tags):
+            temp = []
+            temp2 = []
+            for s_ in range(len_tags):
+                temp.append(viterbi[s_][t-1]*transition_matrix[s_][s]*observation_matrix[word_id][s])
+                temp2.append(viterbi[s_][t-1]*transition_matrix[s_][s])
+            
+            index, value = max(enumerate(temp2), key=operator.itemgetter(1))
+            viterbi[s][t] = max(temp)
+            backpointer[s][t] = index
+        
+    temp2 = []
+    for s_ in range(len_tags):
+        temp2.append(viterbi[s_][T-1]*transition_matrix[s_][len_tags])
     
     
-    
-    
-    
-    
+    index, value = max(enumerate(temp2), key=operator.itemgetter(1))        
+
+    # len_tag+1 means the end node of the graph
+    viterbi[len_tags+1][T] = value
+    backpointer[len_tags+1][T] = index
+    backpointer = np.int64(backpointer)
 
 
+    return viterbi , backpointer
+
+def get_POS_tags (backpointer,sequence,len_tags):
+    T = len(sequence)
+    last_node_id = backpointer[len_tags+1][T]
+    POS_tags = [last_node_id]
+    
+    for i in range(T):
+        previous_node_id = backpointer[last_node_id][T-1-i] 
+        POS_tags.append(previous_node_id)
+        last_node_id = previous_node_id
+    
+    return POS_tags[::-1]
+
+# only works for an example , but it will improves later
+def get_example(input_file):
+    sentence_list = get_original_sentences(input_file)
+    pre_sentence_list,tag_list,word_list = preprocessing(sentence_list)
+    tags, words = give_id(tag_list,word_list)
+    pre_sentence = convert_id(pre_sentence_list,tags,words)
+    len_tags = len(tags)
+    len_words = len(words)
+    transition_matrix,observation_matrix = create_matrices(pre_sentence,len_tags,len_words)
+    
+    # sequence example
+    sequence = [2753,17002]
+    viterbi , backpointer =  viterbi_algorithm(sequence)
+    POS_tags = get_POS_tags (backpointer,sequence,len_tags)
+    print(POS_tags)    
+
+get_example(input_file)
 
 
+#%%    
 
 
 
